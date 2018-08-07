0966a452ceb1b651ab1e77dc30f8aa7779eb1544
ramkumarizaap <ramkumar.izaap@gmail.com>
2018-08-06 22:04:03 -0700

    Shared Module Import

--- src/components/shared-module.ts
+++ src/components/shared-module.ts
@@ -1,5 +1,23 @@
-import {} from '';
+import { NgModule, No_ERRORS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExpandableHeader } from 'scroll/scroll-directive';
+import { ErrorPage } from 'error/error';
+import { FilterPage } from 'filter-page/filter';
+import { HeaderPage } from 'partials/header/header';
+import { FooterPage } from 'partials/footer/footer';
 
-
+@NgModule({
+  imports:[
+    CommonModule,
+  ],
+  exports:[
+    ExpandableHeader,
+    ErrorPage,
+    FilterPage,
+    HeaderPage,
+    FooterPage
+  ],
+  declarations:[ExpandableHeader,ErrorPage,FilterPage,HeaderPage,FooterPage]
+})
 export class SharedModule{
 }
