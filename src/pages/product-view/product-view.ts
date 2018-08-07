97dfe42a5636d15631ce552aa13fd1f7a911799b
ramkumarizaap <ramkumar.izaap@gmail.com>
2018-08-06 21:15:46 -0700

    Product View
    Checking Pincode Status
    Updating Quantity

--- src/pages/product-view/product-view.ts
+++ src/pages/product-view/product-view.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
-import { NavParams, ViewController } from 'ionic-angular';
+import { NavParams, ViewController, AlertController } from 'ionic-angular';
 import { AppSettingsService } from '../../services/app-settings.service';
 import { ProductService } from '../../services/product.service';
+import { UserService } from '../../services/user.service';
 @Component({
   selector: 'page-product-view',
   templateUrl: 'product-view.html'
 })
 export class ProductViewPage {
   baseURL: string;
+  pincodeStatus: string = "";
   productID: number;
+  pincode: number;
+  quantity: number = 1;
   product: any;
   constructor(public appSettings: AppSettingsService,public params: NavParams,
-  public viewCtrl: ViewController,public productService: ProductService){
+  public viewCtrl: ViewController,public productService: ProductService,
+  public userService: UserService,public alertCtrl: AlertController){
     this.baseURL = this.appSettings.getBaseUrl();
     if(this.params.data.id){
       this.productID = this.params.data.id;
@@ -30,5 +35,52 @@ export class ProductViewPage {
 
       console.log('SIngle Product',this.product);
   }
- 
+ qtyUpdate(type: string){
+    if(type === 'add'){
+      if(this.quantity <= 10)
+        this.quantity += 1;
+    }
+    else if(type === 'minus'){
+      if(this.quantity >= 2)
+        this.quantity -= 1;
+    }
+ }
+  checkPincode(){
+    if(this.pincode !== '')
+    {
+      this.userService.checkPincode(this.pincode).subscribe((res)=>{
+        if(res['status'] === 'success')
+          this.pincodeStatus = "This pincode is available to deliver.";
+        else{
+          let err = alertCtrl.create({
+            title:'Error',
+            message: res['msg'],
+            buttons:['OK']
+          });
+          err.present();
+        }
+      });
+    }
+    else{
+      let alert = alertCtrl.create({
+        title:'Error',
+        message:'Please Enter your area pincode',
+        buttons:['OK']
+      });
+      alert.present();
+    }
+  }
+  
+  addToCart(){
+    if(this.pincode !== ''){
+    }
+    else{
+       let alert = alertCtrl.create({
+         title:'Error',
+         message:'Please check your pincode area availability!',
+         buttons:['OK']
+       });
+      alert.present();
+    }
+  }
 }
